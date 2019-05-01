450c3c5d0a80e262ee12f8ed922b5245640b33eb
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2019-05-01 17:16:06 -0400

    include some pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const About = () => {
+  return (
+    <React.Fragment>
+      <h1>About me</h1>
+      <p>I build apps for the web</p>
+    </React.Fragment>
+  )
+}
+
+export default About
---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Blog = () => {
+  return (
+    <React.Fragment>
+      <h1>Blog</h1>
+      <p>Posts will show up here later on.</p>
+    </React.Fragment>
+  )
+}
+
+export default Blog
