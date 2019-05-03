6f877b19487f18ab290ebcbdea0dabdd2c6be858
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2019-05-02 21:00:38 -0400

    install and configure gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,14 @@ module.exports = {
     title: "Full-Stack Bootcamp",
     author: "Edgardo Arriagada",
   },
-  plugins: ["gatsby-plugin-sass"],
+  plugins: [
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "posts",
+        path: `${__dirname}/src/posts`,
+      },
+    },
+  ],
 }
