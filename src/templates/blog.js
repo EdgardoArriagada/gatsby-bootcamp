4721ab592ad59e2a40f84d83069722c614d3d180
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2019-05-06 13:59:56 -0400

    modify blog template to render content from contenful query instead

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -2,26 +2,47 @@ import React from "react"
 
 import Layout from "../components/layout"
 import { graphql } from "gatsby"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+
+// export const query = graphql`
+//   query($slug: String!) {
+//     markdownRemark(fields: { slug: { eq: $slug } }) {
+//       frontmatter {
+//         title
+//         date
+//       }
+//       html
+//     }
+//   }
+// `
 
 export const query = graphql`
   query($slug: String!) {
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      frontmatter {
-        title
-        date
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      publishedDate(formatString: "MMMM Do, YYYY")
+      body {
+        json
       }
-      html
     }
   }
 `
 
 const Blog = props => {
-  const { frontmatter, html } = props.data.markdownRemark
+  const options = {
+    renderNode: {
+      "embedded-asset-block": node => {
+        const { title, file } = node.data.target.fields
+        return <img alt={title["en-US"]} src={file["en-US"].url} />
+      },
+    },
+  }
+  const { title, publishedDate, body } = props.data.contentfulBlogPost
   return (
     <Layout>
-      <h1>{frontmatter.title}</h1>
-      <p>{frontmatter.date}</p>
-      <div dangerouslySetInnerHTML={{ __html: html }} />
+      <h1>{title}</h1>
+      <p>{publishedDate}</p>
+      {documentToReactComponents(body.json, options)}
     </Layout>
   )
 }
