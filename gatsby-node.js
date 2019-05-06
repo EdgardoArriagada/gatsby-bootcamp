75d04cfdf3e868b3b62d5c302fff546082a07181
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2019-05-06 13:59:06 -0400

    configure gatsby-node to create pages from contentful instead

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,17 +1,17 @@
 const path = require("path")
 
-module.exports.onCreateNode = ({ node, actions }) => {
-  const { createNode, createNodeField } = actions
-  if (node.internal.type === "MarkdownRemark") {
-    const slug = path.basename(node.fileAbsolutePath, ".md")
+// module.exports.onCreateNode = ({ node, actions }) => {
+//   const { createNode, createNodeField } = actions
+//   if (node.internal.type === "MarkdownRemark") {
+//     const slug = path.basename(node.fileAbsolutePath, ".md")
 
-    createNodeField({
-      node,
-      name: "slug",
-      value: slug,
-    })
-  }
-}
+//     createNodeField({
+//       node,
+//       name: "slug",
+//       value: slug,
+//     })
+//   }
+// }
 
 module.exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
@@ -19,14 +19,27 @@ module.exports.createPages = async ({ graphql, actions }) => {
   // Query for markdown nodes to use in creating pages.
   // You can query for whatever data you want to create pages for e.g.
   // products, portfolio items, landing pages, etc.
+
+  // const response = await graphql(`
+  //   {
+  //     allMarkdownRemark(limit: 1000) {
+  //       edges {
+  //         node {
+  //           fields {
+  //             slug
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // `)
+
   const response = await graphql(`
     {
-      allMarkdownRemark(limit: 1000) {
+      allContentfulBlogPost {
         edges {
           node {
-            fields {
-              slug
-            }
+            slug
           }
         }
       }
@@ -37,8 +50,8 @@ module.exports.createPages = async ({ graphql, actions }) => {
     throw response.errors
   }
 
-  response.data.allMarkdownRemark.edges.forEach(edge => {
-    const { slug } = edge.node.fields
+  response.data.allContentfulBlogPost.edges.forEach(edge => {
+    const { slug } = edge.node
     createPage({
       // Path for this page — required
       path: `/blog/${slug}`,
